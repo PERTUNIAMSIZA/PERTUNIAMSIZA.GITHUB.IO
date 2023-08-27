f138d0348fa2ac96c7db9a4a60da9912664006e9
Pertunia Msiza <nonhlanhlapertunia112@gmail.com>
2023-08-27 13:24:13 +0200

    Initial commit

---
+++ ComicWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ComicWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
